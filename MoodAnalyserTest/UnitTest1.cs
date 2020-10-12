ca0b81620c258aa724d6f79978b4443f6c4379a0
jayantsaraf <sarafjayant24@gmail.com>
2020-10-13 00:11:20 +0530

    [jayant] solving merge conflict

--- MoodAnalyserTest/UnitTest1.cs
+++ MoodAnalyserTest/UnitTest1.cs
@@ -5,17 +5,6 @@ namespace MoodAnalyserTest
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-<<<<<<< HEAD
-        public void analyseSadMood()
-        {
-            MoodAnalyse analyse = new MoodAnalyse();
-            string message = analyse.analyseMood("Sad");
-            Assert.AreEqual("SAD", message);
-=======
-        public void TestMethod1()
-        {
->>>>>>> UC1_AnalyseMood
-        }
+        
     }
 }
