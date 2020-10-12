cbde19f9e909573b8e1a0eb1df1a7c6d95239079
jayantsaraf <sarafjayant24@gmail.com>
2020-10-12 22:50:05 +0530

    [jayant]Add. added analyseMood function

--- MoodAnalyzer/MoodAnalyzer.cs
+++ MoodAnalyzer/MoodAnalyzer.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyzer
+{
+    public class MoodAnalyse
+    {
+        public string analyseMood(string message)
+        {
+            try
+            {
+                if (message.Contains("sad".ToLower())) ;
+                return "SAD";
+            }
+            catch
+            {
+                return "Happy";
+            }
+        }
+    }
+}
