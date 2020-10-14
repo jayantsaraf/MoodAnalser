be9ea937bc79c7e5e01e9ed0b223c414f9f80af5
jayantsaraf <sarafjayant24@gmail.com>
2020-10-15 00:33:11 +0530

    [jayant]Add. added class and method to use reflection to invoke method

---
+++ MoodAnalyzer/MoodAnalyserReflection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace MoodAnalyzer
+{
+    class MoodAnalyserReflection
+    {
+        public static string CreateObjectAnalyseMood(string message, string methodName)
+        {
+            try
+            {
+                Type type = Type.GetType("MoodAnalyzer.MoodAnalyse");
+                object moodAnalyserObject = MoodAnalyserFactory.CreateMoodAnalyserWithParameters("MoodAnalyzer.MoodAnalyse", "MoodAnalyse", message);
+                MethodInfo method = type.GetMethod(methodName);
+                object mood = method.Invoke(moodAnalyserObject, null);
+                return mood.ToString();
+            }
+            catch (NullReferenceException e)
+            {
+                throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.NO_SUCH_METHOD, "No such method found");
+            }
+        }
+    }
+}
